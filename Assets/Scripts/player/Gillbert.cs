6be677cb07c98ae5f79d8dba028a2924d1f78b58
CasperBohlin <109875785+Exedze@users.noreply.github.com>
2026-04-10 19:08:52 +0200

    waterworks

--- Assets/Scripts/player/Gillbert.cs
+++ Assets/Scripts/player/Gillbert.cs
@@ -7,7 +7,7 @@ public class Gillbert : MonoBehaviour
     private Vector2 playerMoveTrack = new Vector2();
     private float velocity;
     [SerializeField] private Rigidbody2D body;
-   private float speedMod=1;
+   private bool speedMod=true;
     [SerializeField] private float brakingForce = 0.2f;
     [SerializeField] private float gravityStrength=0.05f;
   
@@ -15,12 +15,20 @@ public class Gillbert : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.up = (Vector2)Vector3.Slerp(transform.up, playerMoveTrack, 2*Time.deltaTime);
-        
-        velocity+=playerMoveTrack.magnitude*speedMod*Time.deltaTime;
-        velocity -=velocity*brakingForce*Time.deltaTime*speedMod;
-        
-        transform.position+=transform.up*Time.deltaTime*velocity*speed;
+        //transform.up = (Vector2) Vector3.Slerp(transform.up, playerMoveTrack, 2 * Time.deltaTime);
+
+        if (speedMod)
+        {
+            transform.up = (Vector2) Vector3.Slerp(transform.up, playerMoveTrack, 2 * Time.deltaTime);
+            velocity += playerMoveTrack.magnitude * Time.deltaTime;
+            velocity -= velocity * brakingForce * Time.deltaTime;
+        }
+        else
+        {
+            transform.up = new Vector2(transform.up.x, body.linearVelocity.y+playerMoveTrack.y);
+        }
+
+    transform.position+=transform.up*Time.deltaTime*velocity*speed;
     }
 
     private void OnMove(InputValue value)
@@ -31,14 +39,16 @@ public class Gillbert : MonoBehaviour
 
     public void enterWater()
     {
-        speedMod = 1;
+        speedMod = true;
         body.gravityScale = 0;
+        Debug.Log(body.linearVelocity);
         
+        body.linearVelocity = Vector2.zero;
     }
 
     public void exitWater()
     {
-        speedMod = 0;
+        speedMod = false;
         body.gravityScale = gravityStrength;
     }
 }
