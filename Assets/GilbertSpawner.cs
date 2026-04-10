5d9b6a14d8cfb041f4b93f46fbadfe30de5a13e2
JessicaTheEm2 <a24chrca@student.his.se>
2026-04-10 22:55:35 +0200

    e

---
+++ Assets/GilbertSpawner.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GilbertSpawner : MonoBehaviour
+{
+    public GameObject myGillbert;
+
+    public void SpawnGilbert()
+    {
+        float spawnPointX = Random.Range(-10f, 10f);
+        float spawnPointY = Random.Range(-5f, 5f);
+        Vector2 spawnPosition = new Vector2(spawnPointX, spawnPointY);
+        Vector3 scaleX = new Vector3(Random.Range(0.5f, 20f ), Random.Range(0.5f, 20f ), Random.Range(0.5f, 8f ));
+        //Transform scalePos = new Transform(scaleX);
+        if ((int)Random.Range(0, 2) < 1)
+        {
+            scaleX =new Vector3(-scaleX.x,scaleX.y,scaleX.z);
+        }
+        GameObject gill = Instantiate(myGillbert, spawnPosition, Quaternion.identity);
+     gill.transform.localScale=scaleX;
+    }
+}
