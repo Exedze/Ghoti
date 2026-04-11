781ef7db00f2c13ce5976afd2902678b5ccfbef5
AntonWDSU24 <a24antwe@student.his.se>
2026-04-11 21:53:16 +0200

    Ending

--- Assets/Scripts/TextBubble.cs
+++ Assets/Scripts/TextBubble.cs
@@ -6,20 +6,34 @@ public class TextBubble : MonoBehaviour
 {
 
     public TextMeshProUGUI infoText;
+    public SpriteRenderer spriteToShow;
+    public GameObject collEnd;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
         infoText.enabled = true;
+        if (spriteToShow != null)
+        {
+            spriteToShow.enabled = true;
+            
+        }
+        collEnd.SetActive(true);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         infoText.enabled = false;
+        
     }
 
     private void Start()
     {
         infoText.enabled = false;
+        if (spriteToShow != null)
+        {
+            spriteToShow.enabled = false;
+        }
+        collEnd.SetActive(false);
     }
 }
 
