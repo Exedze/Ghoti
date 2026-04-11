dcca15dd37069b15d4467684de87ca097feefc55
JessicaTheEm2 <a24chrca@student.his.se>
2026-04-11 11:37:41 +0200

    Gillbert spawner updated,

--- Assets/Scripts/GilbertSpawner.cs
+++ Assets/Scripts/GilbertSpawner.cs
@@ -6,10 +6,10 @@ public class GilbertSpawner : MonoBehaviour
     public GameObject gillbertsparent;
     public void SpawnGilbert()
     {
-        float spawnPointX = Random.Range(-10f, 10f);
-        float spawnPointY = Random.Range(-5f, 5f);
+        float spawnPointX = Random.Range(-100f, 2000f);
+        float spawnPointY = Random.Range(-1f, 1000f);
         Vector2 spawnPosition = new Vector2(spawnPointX, spawnPointY);
-        Vector3 scaleX = new Vector3(Random.Range(0.5f, 20f ), Random.Range(0.5f, 20f ), Random.Range(0.5f, 8f ));
+        Vector3 scaleX = new Vector3(Random.Range(100f, 1000f ), Random.Range(200f, 2000f ), Random.Range(0.5f, 8f ));
         //Transform scalePos = new Transform(scaleX);
         if ((int)Random.Range(0, 2) < 1)
         {
