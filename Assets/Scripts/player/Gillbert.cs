6b236a571f68cc55f68ffa1e4a9516d721d05c09
CasperBohlin <109875785+Exedze@users.noreply.github.com>
2026-04-11 03:14:09 +0200

    leap is in

--- Assets/Scripts/player/Gillbert.cs
+++ Assets/Scripts/player/Gillbert.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,51 +6,81 @@ public class Gillbert : MonoBehaviour
 {
     [SerializeField] private float speed=6f;
     private Vector2 playerMoveTrack = new Vector2();
-    private float velocity;
+    private float velocity=0;
     [SerializeField] private Rigidbody2D body;
    private bool speedMod=true;
-    [SerializeField] private float brakingForce = 0.2f;
+    //[SerializeField] private float brakingForce = 0.2f;
     [SerializeField] private float gravityStrength=0.05f;
     [SerializeField] private float boostForce=2;
     private float sb1 = 0;
     private float sb2 = 0;
+    private float movementMulti=1;
+    private Vector3 prevcord = Vector3.zero;
+    private Vector3 delta = Vector3.zero;
+    [SerializeField] private float leapForce = 1;
+    private Vector2 LeapVec=Vector2.zero;
   
 
     // Update is called once per frame
     void Update()
     {
-        //transform.up = (Vector2) Vector3.Slerp(transform.up, playerMoveTrack, 2 * Time.deltaTime);
-
+delta = transform.position - prevcord;
         if (speedMod)
         {
-            transform.up = (Vector2) Vector3.Slerp(transform.up, playerMoveTrack.normalized, 2 * Time.deltaTime);
+            body.AddForce(playerMoveTrack * Time.deltaTime*speed*movementMulti);
+            /*transform.up = (Vector2) Vector3.Slerp(transform.up, playerMoveTrack, 2 * Time.deltaTime);
             velocity += playerMoveTrack.magnitude * Time.deltaTime;
-            velocity -= velocity * brakingForce * Time.deltaTime;
+            velocity -= velocity * brakingForce * Time.deltaTime;*/
         }
         else
         {
-            transform.up = new Vector2(transform.up.x, body.linearVelocity.y+playerMoveTrack.y);
+            if (delta.magnitude < 0.02f&&body.linearVelocity.y==0&&LeapVec!=Vector2.zero)
+            {
+                Debug.Log("once");
+           //add force to push player when they flop
+           body.linearVelocity = new Vector2(LeapVec.x * leapForce * 3, 10 * leapForce);
+           //body.AddForce(new Vector2(LeapVec.x*leapForce*3,10*leapForce));
+            }
+            //transform.up = (Vector2) Vector3.Slerp(transform.up, Vector2.down, 0.5f * Time.deltaTime);
         }
 
-    transform.position+=transform.up*Time.deltaTime*velocity*speed;
-   
+        
+        if (delta.magnitude > 0.02f)
+        {
+            transform.up = Vector3.Slerp(transform.up, transform.position - prevcord, 0.9f);
+        }
+
+        // transform.position+=transform.up*Time.deltaTime*velocity*speed;
+   prevcord = transform.position;
+   if (sb1 > 0 && sb2 > 0)
+   {
+       movementMulti = boostForce;
+   }
+   else
+   {
+       movementMulti = 1;
+           
+   }
     }
 
     private void OnMove(InputValue value)
     {
         if (!speedMod) return;
         playerMoveTrack = value.Get<Vector2>();
-        if (sb1 > 0 && sb2 > 0) playerMoveTrack *= boostForce;
+    }
+    private void OnHop(InputValue value)
+    {
+      //  if (speedMod || delta.magnitude < 0.02f || body.linearVelocity.y == 0) return;
+       // Debug.Log("leap");
+        LeapVec = value.Get<Vector2>();
     }
 
     private void OnSprint(InputValue value)
     {
-      //  Debug.Log(value.Get<float>());
       sb1 = value.Get<float>();
     }
     private void OnSprint1(InputValue value)
     {
-        //Debug.Log(value.Get<float>());
         sb2 = value.Get<float>();
     }
 
@@ -57,14 +88,21 @@ public class Gillbert : MonoBehaviour
     {
         speedMod = true;
         body.gravityScale = 0;
-        Debug.Log(body.linearVelocity);
-        
-        body.linearVelocity = Vector2.zero;
+        playerMoveTrack=Vector2.down;
+      //  Debug.Log(body.linearVelocity);
+      //  playerMoveTrack = transform.up;
+      //  velocity *= 0.5f;
+       //  body.linearVelocity = Vector2.zero;
     }
 
     public void exitWater()
     {
         speedMod = false;
-        body.gravityScale = gravityStrength;
+        //if (transform.up == Vector3.up) {
+          //  Debug.Log("my people need me");
+           // transform.up = (Vector2) Vector3.Slerp(transform.up, Vector2.left, 0.5f * Time.deltaTime);
+        //}
+     body.gravityScale = gravityStrength;
     }
+    
 }
